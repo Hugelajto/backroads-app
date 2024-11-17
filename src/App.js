efbde01c557d212ffe8888a197aa7117a9815d7e
Hugelajto <huonghanna@gmail.com>
2024-11-17 21:26:40 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./componenets/Navbar";
+import Hero from "./componenets/Hero";
+import About from "./componenets/About";
+import Services from "./componenets/Services";
+import Tours from "./componenets/Tours";
+import Footer from "./componenets/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
